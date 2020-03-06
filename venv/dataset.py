53d5f54312ec64a59c40bd42d89fd0644f44c229
Trevor <trevor.h.crow@gmail.com>
2020-03-06 12:07:43 -0500

    working on adding data sets to the project

--- venv/dataset.py
+++ venv/dataset.py
@@ -22,7 +22,6 @@ watson = []
 for i in os.listdir(WATSON_PATH):
     watson.append(np.array(image.open(f'{WATSON_PATH}/{i}')))
 
-
 train_dir = os.path.join(PATH, 'train')
 validation_dir = os.path.join(PATH, 'validation')
 
@@ -116,24 +115,6 @@ model.compile(optimizer='adam',
 
 model.summary()
 
-# pred_model = Sequential([
-#     Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 3)),
-#     MaxPooling2D(),
-#     Conv2D(32, 3, padding='same', activation='relu'),
-#     MaxPooling2D(),
-#     Conv2D(64, 3, padding='same', activation='relu'),
-#     MaxPooling2D(),
-#     Flatten(),
-#     Dense(512, activation='relu'),
-#     Dense(1)
-# ])
-#
-# pred_model.compile(optimizer='adam',
-#                    loss=tf.keras.losses.BinaryCrossentropy(from_logits=True),
-#                    metrics=['accuracy'])
-#
-# pred_model.summary()
-
 history = model.fit_generator(
     train_data_gen,
     steps_per_epoch=total_train // batch_size,
@@ -141,19 +122,6 @@ history = model.fit_generator(
     validation_data=val_data_gen,
     validation_steps=total_val // batch_size
 )
-
-# predict_history = pred_model.fit_generator(
-#     train_data_gen,
-#     steps_per_epoch=total_train // batch_size,
-#     epochs=epochs,
-#     validation_data=predict_data_gen,
-#     validation_steps=total_predict // batch_size
-# )
-# pred_acc = predict_history.history['accuracy']
-# pred_val_acc = predict_history.history['val_accuracy']
-# pred_loss=history.history['loss']
-# pred_val_loss=history.history['val_loss']
-# ^ relates to testing prediction stuff
 
 
 acc = history.history['accuracy']
@@ -164,31 +132,31 @@ val_loss=history.history['val_loss']
 
 epochs_range = range(epochs)
 
-plt.figure(figsize=(8, 8))
-plt.subplot(1, 2, 1)
-plt.plot(epochs_range, acc, label='Training Accuracy')
-plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-plt.legend(loc='lower right')
-plt.title('Training and Validation Accuracy')
-
-plt.subplot(1, 2, 2)
-plt.plot(epochs_range, loss, label='Training Loss')
-plt.plot(epochs_range, val_loss, label='Validation Loss')
-plt.legend(loc='upper right')
-plt.title('Training and Validation Loss')
-
 # plt.figure(figsize=(8, 8))
 # plt.subplot(1, 2, 1)
-# plt.plot(epochs_range, pred_acc, label='Training Accuracy')
-# plt.plot(epochs_range, pred_val_acc, label='Prediction Accuracy')
+# plt.plot(epochs_range, acc, label='Training Accuracy')
+# plt.plot(epochs_range, val_acc, label='Validation Accuracy')
 # plt.legend(loc='lower right')
-# plt.title('Training and Prediction Accuracy')
+# plt.title('Training and Validation Accuracy')
 #
 # plt.subplot(1, 2, 2)
-# plt.plot(epochs_range, pred_loss, label='Prediction Loss')
-# plt.plot(epochs_range, pred_val_loss, label='Prediction Loss')
+# plt.plot(epochs_range, loss, label='Training Loss')
+# plt.plot(epochs_range, val_loss, label='Validation Loss')
 # plt.legend(loc='upper right')
-# plt.title('Training and Prediction Loss')
+# plt.title('Training and Validation Loss')
+
+plt.figure(figsize=(8, 8))
+plt.subplot(1, 2, 1)
+plt.plot(epochs_range, pred_acc, label='Training Accuracy')
+plt.plot(epochs_range, pred_val_acc, label='Prediction Accuracy')
+plt.legend(loc='lower right')
+plt.title('Training and Prediction Accuracy')
+
+plt.subplot(1, 2, 2)
+plt.plot(epochs_range, pred_loss, label='Prediction Loss')
+plt.plot(epochs_range, pred_val_loss, label='Prediction Loss')
+plt.legend(loc='upper right')
+plt.title('Training and Prediction Loss')
 
 plt.show()
 
