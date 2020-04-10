f13a33952a83aa8a56839bad3b8bf7f1812f75c7
Trevor <trevor.h.crow@gmail.com>
2020-04-09 22:35:26 -0400

    fixed img's not showing when they are loaded
    now just need to figure out how to evaluate the image

--- venv/dataset.py
+++ venv/dataset.py
@@ -7,7 +7,6 @@ from PIL import Image, ImageFilter
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-import PIL.Image as image
 
 _URL = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
 
@@ -15,12 +14,18 @@ path_to_zip = tf.keras.utils.get_file('cats_and_dogs.zip', origin=_URL, extract=
 
 PATH = os.path.join(os.path.dirname(path_to_zip), 'cats_and_dogs_filtered')
 
+batch_size = 128
+epochs = 15
+IMG_SIZE = 150 # width and height
+
 # Nicholas' Watson images
 WATSON_PATH = './watson/'
 
 watson = []
 for i in os.listdir(WATSON_PATH):
-    watson.append(np.array(image.open(f'{WATSON_PATH}/{i}')))
+    watson.append(tf.keras.preprocessing.image.load_img(f'{WATSON_PATH}{i}', grayscale=False, color_mode='rgb', target_size=(IMG_SIZE,IMG_SIZE), interpolation='nearest'))
+
+# watson[0].show()
 
 train_dir = os.path.join(PATH, 'train')
 validation_dir = os.path.join(PATH, 'validation')
@@ -37,7 +42,7 @@ num_cats_val = len(os.listdir(validation_cats_dir))
 num_dogs_val = len(os.listdir(validation_dogs_dir))
 
 # Nicholas' Watson images
-num_watson = len(os.listdir(WATSON_PATH))
+num_watson = len(watson)
 
 total_train = num_cats_tr + num_dogs_tr
 total_val = num_cats_val + num_dogs_val
@@ -56,10 +61,6 @@ print("Total training images:", total_train)
 print("Total validation images:", total_val)
 print("Total prediction images:", total_predict)
 
-batch_size = 128
-epochs = 15
-IMG_SIZE = 150 # width and height
-
 train_image_generator = ImageDataGenerator(rescale=1./255) # Generator for our training data
 validation_image_generator = ImageDataGenerator(rescale=1./255) # Generator for our validation data
 prediction_image_generator = ImageDataGenerator(rescale=1./255) # Generator for our prediction data
@@ -74,14 +75,14 @@ val_data_gen = validation_image_generator.flow_from_directory(batch_size=batch_s
                                                               target_size=(IMG_SIZE, IMG_SIZE),
                                                               class_mode='binary')
 
-predict_data_gen = prediction_image_generator.flow_from_directory(batch_size=batch_size,
-                                                                  directory=WATSON_PATH,
-                                                                  shuffle=True,
-                                                                  target_size=(IMG_SIZE, IMG_SIZE),
-                                                                  class_mode='binary')
+# predict_data_gen = prediction_image_generator.flow_from_directory(batch_size=batch_size,
+#                                                                   directory=WATSON_PATH,
+#                                                                   shuffle=True,
+#                                                                   target_size=(IMG_SIZE, IMG_SIZE),
+#                                                                   class_mode='binary')
 
 sample_training_images, _ = next(train_data_gen)
-predict_images, _ = next(predict_data_gen)
+# predict_images, _ = next(predict_data_gen)
 
 # This function will plot images in the form of a grid with 1 row and 5 columns where images are placed in each column.
 def plotImages(images_arr):
@@ -94,8 +95,8 @@ def plotImages(images_arr):
     plt.show()
 
 # shows us the first 5 images of the training images
-# plotImages(sample_training_images[:5])
-# plotImages(predict_images[:5])
+plotImages(sample_training_images[:5])
+plotImages(watson[:5])
 
 model = Sequential([
     Conv2D(16, 3, padding='same', activation='relu', input_shape=(IMG_SIZE, IMG_SIZE, 3)),
@@ -127,39 +128,41 @@ history = model.fit_generator(
 acc = history.history['accuracy']
 val_acc = history.history['val_accuracy']
 
-loss=history.history['loss']
-val_loss=history.history['val_loss']
+loss = history.history['loss']
+val_loss = history.history['val_loss']
 
 epochs_range = range(epochs)
 
-# plt.figure(figsize=(8, 8))
-# plt.subplot(1, 2, 1)
-# plt.plot(epochs_range, acc, label='Training Accuracy')
-# plt.plot(epochs_range, val_acc, label='Validation Accuracy')
-# plt.legend(loc='lower right')
-# plt.title('Training and Validation Accuracy')
-#
-# plt.subplot(1, 2, 2)
-# plt.plot(epochs_range, loss, label='Training Loss')
-# plt.plot(epochs_range, val_loss, label='Validation Loss')
-# plt.legend(loc='upper right')
-# plt.title('Training and Validation Loss')
-
 plt.figure(figsize=(8, 8))
 plt.subplot(1, 2, 1)
-plt.plot(epochs_range, pred_acc, label='Training Accuracy')
-plt.plot(epochs_range, pred_val_acc, label='Prediction Accuracy')
+plt.plot(epochs_range, acc, label='Training Accuracy')
+plt.plot(epochs_range, val_acc, label='Validation Accuracy')
 plt.legend(loc='lower right')
-plt.title('Training and Prediction Accuracy')
+plt.title('Training and Validation Accuracy')
 
 plt.subplot(1, 2, 2)
-plt.plot(epochs_range, pred_loss, label='Prediction Loss')
-plt.plot(epochs_range, pred_val_loss, label='Prediction Loss')
+plt.plot(epochs_range, loss, label='Training Loss')
+plt.plot(epochs_range, val_loss, label='Validation Loss')
 plt.legend(loc='upper right')
-plt.title('Training and Prediction Loss')
+plt.title('Training and Validation Loss')
+
+# plt.figure(figsize=(8, 8))
+# plt.subplot(1, 2, 1)
+# plt.plot(epochs_range, pred_acc, label='Training Accuracy')
+# plt.plot(epochs_range, pred_val_acc, label='Prediction Accuracy')
+# plt.legend(loc='lower right')
+# plt.title('Training and Prediction Accuracy')
+#
+# plt.subplot(1, 2, 2)
+# plt.plot(epochs_range, pred_loss, label='Prediction Loss')
+# plt.plot(epochs_range, pred_val_loss, label='Prediction Loss')
+# plt.legend(loc='upper right')
+# plt.title('Training and Prediction Loss')
 
 plt.show()
 
+evaluation = model.evaluate(watson, batch_size=batch_size)
+print(evaluation)
 with open('itworks.txt', 'r') as f:
     for line in f.readlines():
         print(line)
